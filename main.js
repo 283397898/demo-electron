26d06644182c1e2e9f4844684f37911a549855b5
xiaohu <283397898@qq.com>
2021-12-15 01:08:08 +0800

    11

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow,ipcMain} = require('electron')
 
+ipcMain.on('height',(event, args)=>{
+    console.log(args)
+})
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
         width: 800,
@@ -12,6 +15,9 @@ app.on('ready', () => {
     require('@electron/remote/main').initialize()
     require("@electron/remote/main").enable(mainWindow.webContents)
     mainWindow.loadFile('./src/index.html')
+    mainWindow.webContents.on('did-finish-load',()=>{
+        mainWindow.webContents.send('url','https://www.bilibili.com')
+    })
     mainWindow.webContents.openDevTools()
     mainWindow.on('close', () => {
         mainWindow = null
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,27 @@
 const {BrowserWindow}=require('@electron/remote')
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn').addEventListener('click', () => {
-        let win = new BrowserWindow({
-            width: 800,
-            height: 600,
+const {ipcRenderer} = require('electron')
+const fs = require("fs");
+
+async function getUrl(){
+    return await new Promise((resolve) => {
+        ipcRenderer.on('url', (event, args) => {
+            return resolve(args)
         })
-        win.loadURL('https://www.baidu.com')
     })
-})
+}
+
+window.onload=async()=>{
+    let win = new BrowserWindow({ webPreferences: { offscreen: true } })
+    win.webContents.setFrameRate(30)
+    win.webContents.openDevTools()
+    const url = await getUrl()
+    await win.loadURL(url)
+    win.webContents.on('did-finish-load',()=>{
+        let height=document.body.scrollHeight
+        ipcRenderer.sendSync('height',height)
+    })
+    win.webContents.on('paint', (event, dirty, image) => {
+        fs.writeFileSync('ex.png', image.toPNG())
+    })
+
+}
